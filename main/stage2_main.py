6cacf80152b5f0d8b085264ecc4f85f141b217a0
etally <13945663+etally@user.noreply.gitee.com>
2025-12-22 19:24:56 +0800

    Update NR4DER code

---
+++ main/stage2_main.py
@@ -0,0 +1,102 @@
+import os
+
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from tqdm import tqdm
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+import argparse
+from module.rapid_self import RAPID
+from module.data_load import Rapid_Dataset
+import psutil
+import time
+device = torch.device('cuda:1' if torch.cuda.is_available() else 'cpu')
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--user_num', type=int, default=1855, help='the number of users')
+    parser.add_argument('--ex_num', type=int, default=948, help='the number of exercises')
+    parser.add_argument('--kc_num', type=int, default=57, help='the number of concepts')
+
+    parser.add_argument('--user_hidden_size',type=int, default=64, help='the size of user embedding')
+    parser.add_argument('--ex_hidden_size', type=int, default=64, help='the size of exercise embedding')
+    parser.add_argument('--LSTM_hidden_size', type=int, default=64, help='the size of LSTM hidden state')
+    parser.add_argument('--dropout', type=float, default=0.01, help='the dropout rate')
+    parser.add_argument('--batch_size', type=int, default=12, help='the batch size')
+    parser.add_argument('--epochs', type=int, default=50, help='the number of epochs')
+    parser.add_argument('--lr', type=float, default=0.001, help='the learning rate')
+    parser.add_argument('--reg_lambda', type=float, default=1e-5, help='the regularization parameter')
+    parser.add_argument('--init_rank_len', type=int, default=150, help='the length of initial ranking list')
+    parser.add_argument('--eval_step', type=int, default=1, help='the step of evaluation')
+    parser.add_argument('--output_type', type=str, default='det', choices=['det', 'pro'])
+    parser.add_argument('--Q_matrix_file', type=str, default='../dataset/data_200/nips34/Q.npy')
+    parser.add_argument('--test_data_path', type=str, default='../dataset/data_200/nips34/test_sequences.csv')
+    parser.add_argument('--EB_save_path', type=str, default='../dataset/data_200/nips34/EB_mlstm3_del_0.7.txt')
+    parser.add_argument('--Reranking_result_save_path', type=str, default='../dataset/data_200/nips34/Reranking_result_melt.txt')
+    parser.add_argument('--device', type=str, default='cuda:1')
+
+    args = parser.parse_args()
+    return args
+
+def main(args):
+
+    dataset = Rapid_Dataset(args)
+    test_data = pd.read_csv(args.test_data_path)
+
+    train_dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True,drop_last=True)
+    test_dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=False)
+
+    model = RAPID(args).to(device)
+
+    optimizer = optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.reg_lambda)
+    Q_matrix = torch.tensor(np.load(args.Q_matrix_file)).to(device)
+    Q_matrix = Q_matrix.cpu().numpy()
+
+    for epoch in range(args.epochs):
+        start_time1 = time.time()
+        
+        model.train()
+        model.istrain = True
+        train_loss = 0
+        for batch_data in tqdm(train_dataloader):
+            batch_data = [data.to(device) for data in batch_data]
+            loss, _ = model.forward(batch_data)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            train_loss += loss.item()
+
+        if epoch % args.eval_step == 0:
+            print(f'train memory allocated: {torch.cuda.memory_allocated()}bytes')
+            model.eval()
+            model.istrain = False
+            u_rerank_list = []
+            for batch_data in tqdm(test_dataloader):
+                batch_data = [data.to(device) for data in batch_data]
+                _, u_rerank = model.forward(batch_data)
+                u_rerank_list.append(u_rerank.cpu().numpy())
+
+            u_rerank_list = np.concatenate(u_rerank_list, axis=0)
+            metrics = model.evaluate(u_rerank_list, test_data, Q_matrix)
+
+            uid = test_data['uid'].tolist()
+
+            with open(args.Reranking_result_save_path, 'w') as f:
+                for i in range(len(u_rerank_list)):
+                    rel = u_rerank_list[i]
+                    filter_q = ','.join([str(q) for q in rel])
+                    f.write(f"{uid[i]}\t{filter_q}\n")
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    print(torch.cuda.is_available())
+    main(args)
+
+
+
+
+
---
+++ module/evaluate4ndcg_etc.py
@@ -0,0 +1,134 @@
+import random
+
+import numpy
+import pandas as pd
+import numpy as np
+import torch
+import ast
+from sklearn.metrics import f1_score
+
+'''
+    The evaluation metrics for exercise recommendation refer to the calculation process described in the KDD'2023 MMER paper.
+'''
+
+
+def preprocess_test_data(test_data):
+    stu_true_response = {}
+    test_data['uid'] = [i for i in range(test_data.shape[0])]
+    for i in range(test_data.shape[0]):
+        uid = test_data.iloc[i]['uid']
+        kcs = [int(kc) for kc in test_data.iloc[i]['concepts'].split(',') if kc != '-1']
+        responses = [int(r) for r in test_data.iloc[i]['responses'].split(',') if r != '-1']
+        kc_last_response = {}
+        for kc, r in zip(kcs, responses):
+            kc_last_response[kc] = r
+        stu_true_response[uid] = kc_last_response
+
+    return stu_true_response
+
+
+def preprocess_stu_rec_ex(stu_rec_ex, Q_matrix, stu_true_response):
+    stu_rec_weak_kc = {}
+    for uid in range(stu_rec_ex.shape[0]):
+        had_done_kc = stu_true_response[uid]
+        rec_ex = stu_rec_ex[uid]
+        rec_kc, rec_weak_kc = [], []
+        for qid in rec_ex:
+            kc = np.where(Q_matrix[qid, :] == 1)[0].tolist()
+            rec_kc.extend(kc)
+        for kc in rec_kc:
+            if kc not in rec_weak_kc and kc in had_done_kc:
+                rec_weak_kc.append(kc)
+        stu_rec_weak_kc[uid] = rec_weak_kc
+
+    return stu_rec_weak_kc
+
+def ndcg_at_k(hits, k):
+    if len(hits) < k:
+        return -1
+
+    dcg = np.sum((2 ** np.array(hits[:k]) - 1) / np.log2(np.arange(2, k + 2)))
+
+    sorted_hits = sorted(hits, reverse=True)
+    idcg = np.sum((2 ** np.array(sorted_hits[:k]) - 1) / np.log2(np.arange(2, k + 2)))
+
+    if idcg == 0:
+        return 0.0
+    
+    ndcg = dcg / idcg
+    return ndcg
+
+def Coverage_Fun(stu_rec_ex, Q_matrix):
+    tao_qid = 1 - Q_matrix[stu_rec_ex]
+    tao_qid_tensor = torch.tensor(tao_qid)
+    c_topic_R = torch.prod(tao_qid_tensor, dim=0)
+    c_topic_R = c_topic_R.numpy()
+
+    d_R = 1 - c_topic_R
+
+    return d_R
+
+def diversity(stu_rec_ex, Q_matrix, k):
+    diversity = []
+    for i in range(len(stu_rec_ex)):
+        rec_ex = stu_rec_ex[i][ : k]
+        c_s = Coverage_Fun(rec_ex, Q_matrix)
+        diversity.append(np.sum(c_s))
+    return np.mean(diversity)
+
+def calculate_metrics(stu_true_response, stu_rec_weak_kc, stu_rec_ex, Q_matrix, k=1):
+    hit, ndcg_list, f1 = 0, [], 0
+    hits = []
+    valid_test_stu_num = 0
+    div = diversity(stu_rec_ex, Q_matrix, k)
+
+    for i in range(len(stu_rec_ex)):
+        kc_true_score = {kc: 1 - stu_true_response[i][kc] for kc in stu_true_response[i]}
+        rank_kc = stu_rec_weak_kc[i][:k]
+
+        if len(rank_kc) == 0:
+            continue
+        valid_test_stu_num += 1
+
+        hit_list = [kc_true_score[kc] for kc in rank_kc]
+        hits.append(hit_list)
+
+        hit += sum(hit_list) / len(hit_list)
+
+        temp_ndcg = ndcg_at_k(hit_list, k)
+        if temp_ndcg != -1:
+            ndcg_list.append(temp_ndcg)
+
+        t = [1] * len(hit_list)
+        f1 += f1_score(t, hit_list)
+
+    hit = hit / valid_test_stu_num
+    ndcg = np.mean(ndcg_list)
+    f1 = f1 / valid_test_stu_num
+
+    return hit, f1, ndcg, div, valid_test_stu_num
+
+
+if __name__ == '__main__':
+    dataset = 'nips34'
+    test_file = f'test_sequences.csv'
+    REL_generator_file = f'Reranking_result.txt'
+    Q_matrix = np.load(f'Q.npy')
+
+    test_data = pd.read_csv(test_file)
+    stu_true_response = preprocess_test_data(test_data)
+
+    stu_rec_ex = []
+    with open(REL_generator_file, 'r') as f:
+        for line in f.readlines():
+            stu_id, rec_ex = line.strip().split('\t')
+            rec_ex = [int(qid) for qid in rec_ex.split(',')]
+            stu_rec_ex.append(rec_ex)
+    stu_rec_ex = np.array(stu_rec_ex)
+
+    stu_rec_weak_kc = preprocess_stu_rec_ex(stu_rec_ex, Q_matrix, stu_true_response)
+
+
+    for k in [1, 3, 5, 10]:
+        hit, ndcg, f1, div, valid_test_stu_num = calculate_metrics(stu_true_response, stu_rec_weak_kc, stu_rec_ex, Q_matrix, k=k)
+        print(f'k = {k}, ndcg: {ndcg:.4f}, f1: {f1:.4f}, hit: {hit:.4f}, div: {div:.4f}, valid_test_stu_num: {valid_test_stu_num}')
